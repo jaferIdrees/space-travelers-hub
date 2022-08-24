2d0aad3d87f6fabd53cec3f5aa42c6e43b0e949b
Jafar Yousef <jaferll@gmail.com>
2022-08-24 18:26:30 +0300

    Revert "Fetch missions from API"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Missions from './components/Missions';
-import { retrieveMissions } from './redux/missions/missions';
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(retrieveMissions());
-  });
   return (
     <Routes>
       <Route path="/" element={<Missions />} />
--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import MainNavbar from './components/Navbar';
-import missionsStore from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <>
     <MainNavbar />
-    <Provider store={missionsStore}>
-      <BrowserRouter>
-        <div className="mainContainer">
-          <App />
-        </div>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <div className="mainContainer">
+        <App />
+      </div>
+    </BrowserRouter>
 
   </>,
 
