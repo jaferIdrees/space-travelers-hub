0d77d974b83ecae21c7882da1c1abaeb826da058
jaferIdrees <jaferll@gmail.com>
2022-08-24 18:24:35 +0300

    Fetch missions from API

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
+import { v4 as uuidv4 } from 'uuid';
+import { useSelector } from 'react-redux';
 
 function Missions() {
+  const state = useSelector((state) => state.missionsReducer);
   return (
-    <p>Missions</p>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        {state.map((mission) => (
+          <tr key={uuidv4()}>
+            <td>{mission.mission_name}</td>
+            <td>{mission.description}</td>
+            <td>{mission.mission_name}</td>
+          </tr>
+        ))}
+        {' '}
+      </tbody>
+    </Table>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import MainNavbar from './components/Navbar';
+import missionsStore from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <>
     <MainNavbar />
-    <BrowserRouter>
-      <div className="mainContainer">
-        <App />
-      </div>
-    </BrowserRouter>
+    <Provider store={missionsStore}>
+      <BrowserRouter>
+        <div className="mainContainer">
+          <App />
+        </div>
+      </BrowserRouter>
+    </Provider>
 
   </>,
 
